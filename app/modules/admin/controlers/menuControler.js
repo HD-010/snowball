b3094fc6090929bcb392be2e9377e71da3739d8b
e01 <912744609@qq.com>
2019-06-01 17:44:08 +0800

    优化

--- app/modules/admin/controlers/menuControler.js
+++ app/modules/admin/controlers/menuControler.js
@@ -2,17 +2,7 @@ function menuControler(){
     var that = this;
 
     /**
-     * 获取顶级栏目
-     */
-    this.top = function(){
-        var menu = this.model("Menu");
-        menu.lists({},function(results){
-            that.renderJson(results);
-        });
-    }
-
-    /**
-     * 获取栏目下的子菜单
+     * 获取栏目列表
      */
     this.type = function(){
         var menu = this.model("Menu");
