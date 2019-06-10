38840cdc677e01fc661dfcccd114d07990699592
tianxin <3520605387@qq.com>
2019-06-10 18:00:19 +0800

    group

---
+++ app/modules/admin/controlers/groupControler.js
@@ -0,0 +1,13 @@
+function GroupControler(){
+    var that = this;
+
+    /**
+     * 获取栏目列表
+     */
+    this.addGroup = function(){       
+        that.render({});  
+    }
+
+}
+
+module.exports = GroupControler;
