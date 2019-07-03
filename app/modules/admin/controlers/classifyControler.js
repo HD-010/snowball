adcd25f4317d811a1857fd7861622c7b9ee9c828
e01 <912744609@qq.com>
2019-07-03 14:51:05 +0800

    添加可延展性属性

---
+++ app/modules/admin/controlers/classifyControler.js
@@ -0,0 +1,33 @@
+function classifyControler(){
+    var that = this;
+    /**
+     * 分类列表
+     */
+    this.index = function(){
+        that.render({});
+    }
+
+    /**
+     * 添加分类
+     */
+    this.add = function(){
+        that.render({});
+    }
+
+    /**
+     * 编辑分类
+     */
+    this.edt = function(){
+        that.render({});
+    }
+
+    /**
+     * 删除分类
+     */
+    this.del = function(){
+        that.render({});
+    }
+
+}
+
+module.exports = classifyControler;
