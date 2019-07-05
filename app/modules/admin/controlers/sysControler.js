f5308d38e101f295a678dc427a44dd3997365980
tianxin <3520605387@qq.com>
2019-07-05 17:59:29 +0800

    上传设置开始

--- app/modules/admin/controlers/sysControler.js
+++ app/modules/admin/controlers/sysControler.js
@@ -1,47 +1,59 @@
 function sysControler(){
     var that = this;
     // 基本设置
-    this.bases = function(){ 
-        var process = this.model("DataProcess");
+    that.bases = function(){ 
+        var process = that.model("DataProcess");
         var userInfor = process.getUserInfo();
         var ppermit = process.getUserInfo('PPER',1)
         that.render({});
     }
 
     //安全设置
-    this.securitySet = function(){
+    that.securitySet = function(){
         that.render({});
     }
     
     //验证码设置
-    this.code = function(){
+    that.code = function(){
         that.render({});
     }
 
      //logo设置
-     this.logo = function(){
+     that.logo = function(){
         that.render({});
     }
   
     //地图设置
-    this.mapSet = function (){
+    that.mapSet = function (){
         that.render({});
     }
     
   
     //缓存设置
-    this.cache = function (){
+    that.cache = function (){
          that.render({});
     }
 
     //上传设置
-    this.upload = function (){
-        that.render({});
+    that.upload = function (){
+        // var sys = that.model("Sys")
+        // sys.updateSet(function(){
+            that.render({});
+        //});
+        
+    }
+
+    //保存上传设置
+    that.saveUpdateSet = function(){
+        var sys = that.model("Sys")
+        sys.saveUpdateSet(function(res){
+            that.renderJson(res);
+        });
     }
 
    //友情链接
-   this.flink = function (){
-       var sys = this.model("Sys")
+   that.flink = function (){
+       var sys = that.model("Sys")
        sys.getFlink(function(res){
             that.render({list:res});
        });        
@@ -51,7 +63,7 @@ function sysControler(){
     that.addFlink = function(){
         var data = {};
         var oid = that.GET('oid') || that.POST('oid');
-        data.normalAsync = this.plug('Uploads',{
+        data.normalAsync = that.plug('Uploads',{
           url:'/admin/sys/ajaxImg?oid=' + oid,      //上传文件的服务端地址
           control:'#fileInput',                   //载入文件的input框id
           form:'#testForm',                       //承载input框的form元素的id
@@ -73,6 +85,60 @@ function sysControler(){
             typeDir: true        //需要分类管理文件（按日期分类）
         });
         
+    }
+
+
+    /**
+     * 保存友情连接信息
+     */
+    that.saveFlink = function(){
+        var sys = that.model("Sys");
+        sys.saveFlink(function(res){
+            that.renderJson(res);
+        })
+    }
+
+    /**
+     * 删除友情链接
+     */
+    that.delFlink = function(){
+        var sys = that.model("Sys");
+        sys.delFlink(function(res){
+            that.renderJson(res);
+        })
+    }
+
+    /**
+     * 编辑友情链接信息
+     * 
+     */
+    that.editFlink = function(){
+        var data = {};
+        
+        var oid = that.GET('oid') || that.POST('oid');        
+        data.normalAsync = that.plug('Uploads',{
+          url:'/admin/sys/ajaxImg?oid=' + oid,      //上传文件的服务端地址
+          control:'#fileInput',                   //载入文件的input框id
+          form:'#testForm',                       //承载input框的form元素的id
+          successCallback:'imgfun',
+          faileCallback:'errorfun',
+      }).normalAsync;
+        var sys = that.model("Sys");
+
+        sys.getFlink(function(res){
+            data.list = res;
+            that.render(data);
+        })
+    }
+
+    /**
+     * 修改友情链接信息
+     */
+    that.updateFlink = function(){
+        var sys = that.model("Sys");
+        sys.saveFlink(function(res){
+            that.renderJson(res);
+        })
     }
 
 }
--- app/modules/admin/models/SysModel.js
+++ app/modules/admin/models/SysModel.js
@@ -1,5 +1,19 @@
 function SysModel(){
     var that = this;
+
+
+    that.getSysInfo = function(tagname){
+        //根据网站基本配置信息
+        var condition = {
+            table:["youbang_sys_option"],//查询的表名
+            where:[],           //更新条件
+          
+        };
+        if(tagname) condition.where.push(" tagname= '"+tagname+"'");
+        that.DB().log().get(condition,function(error,res){
+            if(res.length) return res;
+        });
+    }
     
     /**
      * 友情链接信息查新
@@ -18,5 +32,96 @@ function SysModel(){
             if(res.length) callback(res);
         });
     }
+
+    /**
+     * 保存友情链接信息
+     */
+    that.saveFlink = function(callback){
+        var data = {};
+          //获取插入信息
+        data.id = this.POST('id');
+        data.litpic = this.POST('litpic');
+        data.title = this.POST('title');
+        data.linkurl = this.POST('linkurl');
+        data.enable = this.POST('enable') || 0;
+       console.log(data);
+       var condition = {
+            table:["youbang_flink"],                                 //查询的表名
+            fields:[{
+                "litpic":data.litpic,
+                "title":data.title,
+                "linkurl":data.linkurl,
+                "enable":data.enable,
+                "addtime":'NOW()',
+            }],
+            where:[],         //被查询的字段名称（别名在此指定）
+         
+        };
+        if(data.id) condition.where.push(" id = "+data.id);
+        that.DB().log().set(condition,function(error,results){
+            if(results.insertId){
+               if(data.id){
+                    var obj={
+                        message:"友情链接修改成功!",
+                        uri:"/admin/sys/flink",
+                        error:0
+                    }
+                    callback(obj)
+               }else{
+                var obj={
+                        message:"友情链接添加成功!",
+                        uri:"/admin/sys/flink",
+                        error:0
+                    }
+                    callback(obj)
+               }
+            }
+        })
+    }
+
+    /**
+     * 删除友情信息
+     * 条件 根据友情链接ID 删除
+     */
+    that.delFlink = function(callback){
+        var data = {};
+        data.id = that.GET("id");
+        if(data){
+            var condition = {
+                table:["youbang_flink"],          //查询的表名
+                where:[]          //更新条件              
+            };
+            condition.where.push(" id = "+data.id);
+            that.DB().del(condition,function(error,results){
+                if(results.affectedRows){
+                    var obj={
+                        message:"友情链接删除成功!",
+                        uri:"/admin/sys/flink",
+                        error:0
+                    }
+                    callback(obj)                
+                }
+            })
+        }
+
+    }
+    /**
+     * 文件及图片的上传设置
+     */
+    that.updateSet = function(callback){
+        var tagname  = "SYS_UPDATE";
+        console.log(that.getSysInfo(tagname))
+    }
+
+    /**
+     * 文件及图片的上传设置
+     */
+    that.saveUpdateSet = function(callback){
+        var tagname  = "SYS_UPDATE";
+        var data = {};
+        data.updateData = that.POST("!updateData");
+        console.log("上传设置",data.updateData);
+    }
+
 }
 module.exports = SysModel;
