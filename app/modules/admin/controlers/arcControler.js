fd8346e1c0aff49495d698e94e7c5facb1109071
e01 <912744609@qq.com>
2019-07-10 15:51:17 +0800

    增加内容发布缩略图上传功能

--- app/modules/admin/controlers/arcControler.js
+++ app/modules/admin/controlers/arcControler.js
@@ -47,6 +47,36 @@ function arcControler(){
             
             data.addInfo = res.results[0].fieldset;  //附加表字段信息
             data.addlist = res.results[0].listfields.split(',');
+
+             // 获取前端逻辑处理代码
+             data.cropperView = that.plug('Uploads',{
+                accept         : 'image/jpg,image/jpeg,image/png',     //在弹窗中可以选择的文件类型
+                cropper_css    : '/stylesheets/lib/cropper.min.css',
+                imgCropping_css: '/stylesheets/lib/ImgCropping.css',
+                cropper_js     : '/javascripts/lib/cropper.min.js'
+            }).cropperView;
+            // 获取前端逻辑处理代码
+            var oid = that.GET('oid') || that.POST('oid');
+            data.cropperAsync = that.plug('Uploads',{
+                aspectRatio   : 1 / 1,           //默认比例
+                viewMode      : 1,
+                preview       : '.previewImg',   //预览视图
+                guides        : true,            //裁剪框的虚线(九宫格)
+                autoCropArea  : 0.8,             //0-1之间的数值，定义自动剪裁区域的大小，默认0.8
+                movable       : true,            //是否允许移动图片
+                dragCrop      : true,            //是否允许移除当前的剪裁框，并通过拖动来新建一个剪裁框区域
+                movable       : true,            //是否允许移动剪裁框
+                resizable     : false,           //是否允许改变裁剪框的大小
+                zoomable      : true,            //是否允许缩放图片大小
+                mouseWheelZoom: true,            //是否允许通过鼠标滚轮来缩放图片
+                touchDragZoom : true,            //是否允许通过触摸移动来缩放图片
+                rotatable     : false,           //是否允许旋转图片
+                responsive    : false,
+                crop          : function(e) {
+                    console.log(e)// 输出结果数据裁剪图像。
+                },
+                url: '/admin/upload/img?oid='+oid,   //上传图片的服务地址
+            }).cropperAsync;
             ps = that.testRender(data,ps);
         });
     }
@@ -207,6 +237,35 @@ function arcControler(){
                 }
                 data.error = res.error;
                 data.data = res.results;
+                // 获取前端逻辑处理代码
+                data.cropperView = that.plug('Uploads',{
+                    accept         : 'image/jpg,image/jpeg,image/png',     //在弹窗中可以选择的文件类型
+                    cropper_css    : '/stylesheets/lib/cropper.min.css',
+                    imgCropping_css: '/stylesheets/lib/ImgCropping.css',
+                    cropper_js     : '/javascripts/lib/cropper.min.js'
+                }).cropperView;
+                // 获取前端逻辑处理代码
+                var oid = that.GET('oid') || that.POST('oid');
+                data.cropperAsync = that.plug('Uploads',{
+                    aspectRatio   : 1 / 1,           //默认比例
+                    viewMode      : 1,
+                    preview       : '.previewImg',   //预览视图
+                    guides        : true,            //裁剪框的虚线(九宫格)
+                    autoCropArea  : 0.8,             //0-1之间的数值，定义自动剪裁区域的大小，默认0.8
+                    movable       : true,            //是否允许移动图片
+                    dragCrop      : true,            //是否允许移除当前的剪裁框，并通过拖动来新建一个剪裁框区域
+                    movable       : true,            //是否允许移动剪裁框
+                    resizable     : false,           //是否允许改变裁剪框的大小
+                    zoomable      : true,            //是否允许缩放图片大小
+                    mouseWheelZoom: true,            //是否允许通过鼠标滚轮来缩放图片
+                    touchDragZoom : true,            //是否允许通过触摸移动来缩放图片
+                    rotatable     : false,           //是否允许旋转图片
+                    responsive    : false,
+                    crop          : function(e) {
+                        console.log(e)// 输出结果数据裁剪图像。
+                    },
+                    url: '/admin/upload/img?oid='+oid,   //上传图片的服务地址
+                }).cropperAsync;
                 ps = that.testRender(data);
             });
         });
