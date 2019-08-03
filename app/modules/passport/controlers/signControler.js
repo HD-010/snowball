77db0a2e63dc29510a39fef95efb042a7dc1805d
e01 <912744609@qq.com>
2019-08-03 20:52:38 +0800

    修复登录时错帐号不能正常提示的问题

--- app/modules/passport/controlers/signControler.js
+++ app/modules/passport/controlers/signControler.js
@@ -16,15 +16,8 @@ function signControler(){
         var dataProcess = this.model("DataProcess");
         var userInfor = this.model('UserInfor');
         userInfor.infors({}, function(res) {
-            if (res.error) return that.render(res.data,true);
+            if (res.error) return that.render(res,true);
             //校验密码
-            if (res.data.length === 0) {
-                return that.render({
-                    error: 1,
-                    uri: '/admin/sign/_in',
-                    message: "登录的帐户不存在！"
-                });
-            }
             var infors = res.data[0];
             res = dataProcess.loginValid(res.data);
             if(res.error) return that.renderJson(res);
