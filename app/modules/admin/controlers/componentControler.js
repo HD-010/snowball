06d29951a62e16fcd25def21ae6e445c010c8f8f
e01 <912744609@qq.com>
2019-08-27 17:56:21 +0800

    优化图片上传

--- app/modules/admin/controlers/componentControler.js
+++ app/modules/admin/controlers/componentControler.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-06-15 14:22:07
- * @LastEditTime: 2019-08-27 17:33:40
+ * @LastEditTime: 2019-08-27 17:55:47
  * @LastEditors: Please set LastEditors
  */
 function componentControler(){
@@ -29,6 +29,7 @@ function componentControler(){
 
     //编辑应用
     this.edit = function(){
+        var data = {error: 0};
         var cid = this.GET('cid');
         if(!cid) return this.render({},"/err404");
         var component = that.model('Component');
@@ -42,8 +43,8 @@ function componentControler(){
         //获取组件信息
         var param = {limit: 1,id: cid};
         component.list(param,function(res){
-            
-            that.render(res);
+            data = mergeObj([data, res]);
+            that.render(data);
         });
     }
 
