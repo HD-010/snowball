63f74514c2f0e417604522d16111599d71c1f49b
e01 <912744609@qq.com>
2019-08-27 17:42:09 +0800

    优化图片上传

--- app/modules/admin/controlers/arcControler.js
+++ app/modules/admin/controlers/arcControler.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-06-19 15:07:29
- * @LastEditTime: 2019-08-27 09:21:33
+ * @LastEditTime: 2019-08-27 17:39:11
  * @LastEditors: Please set LastEditors
  */
 /**
@@ -71,35 +71,11 @@ function arcControler(){
 
             data.addoninfos = res.results[0].addoninfos;              //附加表字段信息
             data.comname = res.results[0].comname;              //附加表字段信息
-            // 获取前端逻辑处理代码
-            data.cropperView = that.plug('Uploads',{
-                accept         : 'image/jpg,image/jpeg,image/png',     //在弹窗中可以选择的文件类型
-                cropper_css    : '/stylesheets/lib/cropper.min.css',
-                imgCropping_css: '/stylesheets/lib/ImgCropping.css',
-                cropper_js     : '/javascripts/lib/cropper.min.js'
-            }).cropperView;
-            // 获取前端逻辑处理代码
+            //获取上传文件组件信息
             var oid = that.GET('oid') || that.POST('oid');
-            data.cropperAsync = that.plug('Uploads',{
-                aspectRatio   : 1 / 1,           //默认比例
-                viewMode      : 1,
-                preview       : '.previewImg',   //预览视图
-                guides        : true,            //裁剪框的虚线(九宫格)
-                autoCropArea  : 0.8,             //0-1之间的数值，定义自动剪裁区域的大小，默认0.8
-                movable       : true,            //是否允许移动图片
-                dragCrop      : true,            //是否允许移除当前的剪裁框，并通过拖动来新建一个剪裁框区域
-                movable       : true,            //是否允许移动剪裁框
-                resizable     : false,           //是否允许改变裁剪框的大小
-                zoomable      : true,            //是否允许缩放图片大小
-                mouseWheelZoom: true,            //是否允许通过鼠标滚轮来缩放图片
-                touchDragZoom : true,            //是否允许通过触摸移动来缩放图片
-                rotatable     : false,           //是否允许旋转图片
-                responsive    : false,
-                crop          : function(e) {
-                    console.log(e)// 输出结果数据裁剪图像。
-                },
-                url: '/admin/upload/img?oid='+oid,   //上传图片的服务地址
-            }).cropperAsync;
+            data = mergeObj([data, this.model("File").upload({
+                oid: oid
+            })]);
             ps = that.testRender(data,ps);
         });
     }
--- app/modules/pc/controlers/shopControler.js
+++ app/modules/pc/controlers/shopControler.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-05-27 09:47:19
+ * @LastEditTime: 2019-08-27 17:41:47
+ * @LastEditors: Please set LastEditors
+ */
 function shopControler(){
     /**
      * 解析店铺
@@ -244,33 +251,11 @@ function shopControler(){
         var data = {error:0,title:"订单评价"};
         var self = this;
         var params = {};
-        params.cropperView = this.plug('Uploads',{
-            accept         : 'image/jpg,image/jpeg,image/png',     //在弹窗中可以选择的文件类型
-            cropper_css    : '/stylesheets/lib/cropper.min.css',
-            imgCropping_css: '/stylesheets/lib/ImgCropping.css',
-            cropper_js     : '/javascripts/lib/cropper.min.js'
-        }).cropperView;
-        // 获取前端逻辑处理代码
-        params.cropperAsync = this.plug('Uploads',{
-            aspectRatio   : 1 / 1,           //默认比例
-            viewMode      : 1,
-            preview       : '.previewImg',   //预览视图
-            guides        : true,            //裁剪框的虚线(九宫格)
-            autoCropArea  : 0.8,             //0-1之间的数值，定义自动剪裁区域的大小，默认0.8
-            movable       : true,            //是否允许移动图片
-            dragCrop      : true,            //是否允许移除当前的剪裁框，并通过拖动来新建一个剪裁框区域
-            movable       : true,            //是否允许移动剪裁框
-            resizable     : false,           //是否允许改变裁剪框的大小
-            zoomable      : true,            //是否允许缩放图片大小
-            mouseWheelZoom: true,            //是否允许通过鼠标滚轮来缩放图片
-            touchDragZoom : true,            //是否允许通过触摸移动来缩放图片
-            rotatable     : false,           //是否允许旋转图片
-            responsive    : false,
-            crop          : function(e) {
-                console.log("=========AAAAAAAAAA:",e)// 输出结果数据裁剪图像。
-            },
-            url: '/h5/upload/img',   //上传图片的服务地址
-        }).cropperAsync;
+        //获取上传文件组件信息
+        var oid = that.GET('oid') || that.POST('oid');
+        data = mergeObj([data, this.model("File").upload({
+            oid: oid
+        })]);
 
         //传入视图参数
         data.params = params;
@@ -428,33 +413,11 @@ function shopControler(){
         var data = {error:0,title:"申请退款"};
         var self = this;
         var params = {};
-        params.cropperView = this.plug('Uploads',{
-            accept         : 'image/jpg,image/jpeg,image/png',     //在弹窗中可以选择的文件类型
-            cropper_css    : '/stylesheets/lib/cropper.min.css',
-            imgCropping_css: '/stylesheets/lib/ImgCropping.css',
-            cropper_js     : '/javascripts/lib/cropper.min.js'
-        }).cropperView;
-        // 获取前端逻辑处理代码
-        params.cropperAsync = this.plug('Uploads',{
-            aspectRatio   : 1 / 1,           //默认比例
-            viewMode      : 1,
-            preview       : '.previewImg',   //预览视图
-            guides        : true,            //裁剪框的虚线(九宫格)
-            autoCropArea  : 0.8,             //0-1之间的数值，定义自动剪裁区域的大小，默认0.8
-            movable       : true,            //是否允许移动图片
-            dragCrop      : true,            //是否允许移除当前的剪裁框，并通过拖动来新建一个剪裁框区域
-            movable       : true,            //是否允许移动剪裁框
-            resizable     : false,           //是否允许改变裁剪框的大小
-            zoomable      : true,            //是否允许缩放图片大小
-            mouseWheelZoom: true,            //是否允许通过鼠标滚轮来缩放图片
-            touchDragZoom : true,            //是否允许通过触摸移动来缩放图片
-            rotatable     : false,           //是否允许旋转图片
-            responsive    : false,
-            crop          : function(e) {
-                console.log("=========AAAAAAA:",e)// 输出结果数据裁剪图像。
-            },
-            url: '/h5/upload/img',   //上传图片的服务地址
-        }).cropperAsync;
+        //获取上传文件组件信息
+        var oid = that.GET('oid') || that.POST('oid');
+        data = mergeObj([data, this.model("File").upload({
+            oid: oid
+        })]);
 
         //传入视图参数
         data.params = params;
