e6dcdc4dc1c2eeb6b89ee79354881a2758af6cfe
e01 <912744609@qq.com>
2019-09-21 18:53:51 +0800

    删除modes_

--- app/modules_/api/controlers/errController.js
+++
@@ -1,9 +0,0 @@
-function errControler(){
-    var that = this;
-    this._404 = function(){
-        console.log("123465464")
-        return that.renderJson({error:"没有此接口"});
-    }
-}
-
-module.exports = errControler;
--- app/modules_/pc/controlers/errControler.js
+++
@@ -1,8 +0,0 @@
-function errControler(){
-    var that = this;
-    this._404 = function(){
-        return this.render({},'/err404');
-    }
-}
-
-module.exports = errControler;
