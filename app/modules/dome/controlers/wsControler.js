938eab893f1dc056bf6fce7d27e20b3c2a401b62
e01 <912744609@qq.com>
2019-09-25 21:53:51 +0800

    增加webSocket接口

--- app/modules/dome/controlers/wsControler.js
+++ app/modules/dome/controlers/wsControler.js
@@ -1,9 +1,15 @@
 function wsControler(){
 	
 	this.t1 = function(){
-		console.log("00000000000000000000000000000000000000000");
 		//console.log(this.res)
-		this.renderJson(this.req.body)
+		//clients[this.req.curCliId].send()；//向当前客户端发送信息
+		var cli = uni2cli(34);				//获取uniId为34的客户端
+		if(cli) return cli.send(JSON.stringify(this.req.body)); 	//向指定客户端发送信息
+		clients[this.req.curCliId].send(JSON.stringify({error: 1, message:'对象商户端不在线！'}))
+		// for(var a in clients){
+		// 	clients[a].send(JSON.stringify(this.req.body))
+		// }
+		//this.renderJson(this.req.body)
 	}
 }
 
---
+++ app/ws测试参数.md
@@ -0,0 +1,5 @@
+{action:'/dome/ws/t1',data:{a:'dsdfsdyuif'}}
+
+ws://127.0.0.1:3005
+
+echo-protocol
