69c9b613de9c5766b6c4553cd6dc9d91df21fb10
e01 <912744609@qq.com>
2019-09-29 19:01:45 +0800

    完善接口说明

--- app/modules/api/controlers/taskControler.js
+++ app/modules/api/controlers/taskControler.js
@@ -11,21 +11,43 @@ function taskControler() {
 	this.list = function() {
 		//任务列表数据模型说明：
 		var	data = {
-			error: 0,					//任务请求状态0，成功；1失败
-			message: "成功获取任务列表",  //任务请求状态描述
-			type: "task_list",			//请求类型标识
-			persistent: true,			//true持久任务，会被保存到客户端（应用场景：如执行完插播任务后，接着执行当前任务); fals 插播任务
-			list: [						//任务列表
+			//任务请求状态0，成功；1失败
+			error: 0,
+			//任务请求状态描述
+			message: "成功获取任务列表",  
+			//type 请求类型标识， 可有以下值
+			//task_list 要求执行任务列表
+			//oper_play 要求屏幕执行播放
+			//oper_play 要求屏幕停止播放
+			type: "task_list",
+			//任务持久标识persistent 可有以下值
+			//true持久任务，会被保存到客户端（应用场景：如执行完插播任务后，接着执行当前任务); 
+			//false 插播任务,任务列表不会被保存到客户端
+			persistent: true,	false,
+			//任务分区表
+			list: [						
 				//一区播放任务
 				{
+					//type 任务成员类型，可有以下值 
+					//img  任务成员为图片
+					//video 任务成员为视频。可支持本地视频(mp4/flv)，网络视频地址（mp4/flv/m3u8）及流媒体（rtmp/hls/rtsp）
+					//notice
 					type: 'img', //播放图片
+					//taskTag 任务成员标识
 					taskTag: 't1',
-					style: "",
+					//style 任务成员样式
+					style: {width:'20%',height:'33.3%',right:0},
+					//enable 任务成员是否启用，
+					//true 启用，客户端展示当前成员
+					//false 不启用，客户端不展示当前成员
 					enable: true,
+					//style 任务成员元素列表
 					list: [
 						{
-							url:'/img/01.png',		//资源地址
-							duration: 2  			//播放时长（单位：秒）
+							//资源地址
+							url:'/img/01.png',	
+							//播放时长（单位：秒）
+							duration: 2  			
 						},
 						{
 							url:'/img/02.png',
@@ -41,7 +63,7 @@ function taskControler() {
 				{
 					type: 'img', //播放图片
 					taskTag: 't4',
-					style: '',
+					style: {width:'20%',height:'33.3%',right:0,top:'33.3%'},
 					enable: true,
 					list: [
 						{
@@ -62,7 +84,7 @@ function taskControler() {
 				{
 					type: 'img', //播放图片
 					taskTag: 't5',
-					style: '',
+					style: {width:'20%',height:'33.3%',right:0,top:'66.6%'},
 					enable: true,
 					list: [
 						{
@@ -80,10 +102,14 @@ function taskControler() {
 					]
 				},
 				//二区播放任务
+				/**
+				 * 视频播放数据模型
+				 * 样式说明: 客户端定位position: static
+				 */
 				{
 					type: 'video', //播放图片
 					taskTag: 't2',
-					style: "",
+					style: {'height':'80%', 'width':'80%'},
 					enable: true,
 					list: [
 						{
@@ -104,7 +130,7 @@ function taskControler() {
 				{
 					type: 'notice', //播放图片
 					taskTag: 't3',
-					style: '',
+					style: {'height':'20%', 'width':'80%','top':'80%'},
 					enable: true,
 					list: [
 						{
@@ -125,29 +151,29 @@ function taskControler() {
 					]
 				},
 				//三区播放任务
-				{
-					type: 'notice', //播放图片
-					taskTag: 't6',
-					style: '',
-					enable: true,
-					list: [
-						{
-							title:'祝70周年国庆4',
-							content:'中华人民共和国万岁！中华人民共和国万岁！',
-							duration: 20
-						},
-						{
-							title:'祝70周年国庆5',
-							content:'中华人民共和国万岁,万岁！',
-							duration: 20
-						},
-						{
-							title:'祝70周年国庆6',
-							content:'中华人民共和国万岁,万岁,万万岁！',
-							duration: 20
-						}
-					]
-				}
+				// {
+				// 	type: 'notice', //播放图片
+				// 	taskTag: 't6',
+				// 	style: '',
+				// 	enable: true,
+				// 	list: [
+				// 		{
+				// 			title:'祝70周年国庆4',
+				// 			content:'中华人民共和国万岁！中华人民共和国万岁！',
+				// 			duration: 20
+				// 		},
+				// 		{
+				// 			title:'祝70周年国庆5',
+				// 			content:'中华人民共和国万岁,万岁！',
+				// 			duration: 20
+				// 		},
+				// 		{
+				// 			title:'祝70周年国庆6',
+				// 			content:'中华人民共和国万岁,万岁,万万岁！',
+				// 			duration: 20
+				// 		}
+				// 	]
+				// }
 			]
 		}
 		this.renderJson(data);
